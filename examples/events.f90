5ae70e11856ac164e961e4cc9571267880f6686c
interkosmos <pengel@hs-nb.de>
2018-02-03 23:57:32 +0100

    Minor updates.

--- examples/events.f90
+++ examples/events.f90
@@ -9,47 +9,71 @@
 ! Build with:
 ! $ gfortran8 -o events -Wl,-rpath=/usr/local/lib/gcc8/ -I/usr/local/include/ -L/usr/local/lib/ -lX11 events.f90 xlib.o
 program main
-    use iso_c_binding
+    use, intrinsic :: iso_c_binding
     use xlib
     use xlib_consts
     use xlib_types
     implicit none
-    type(c_ptr)           :: display
-    type(c_ptr)           :: gc
-    type(x_event), target :: event
-    type(x_gc_values)     :: values
-    integer               :: screen
-    integer(kind=8)       :: root
-    integer(kind=8)       :: window
-    integer(kind=8)       :: black
-    integer(kind=8)       :: white
+    type(c_ptr)       :: display
+    type(c_ptr)       :: gc
+    type(x_event)     :: event
+    type(x_gc_values) :: values
+    integer           :: screen
+    integer           :: status
+    integer(kind=8)   :: root
+    integer(kind=8)   :: window
+    integer(kind=8)   :: black
+    integer(kind=8)   :: white
+    integer(kind=8)   :: wm_delete_window
+    type(x_configure_event) :: xce
 
+    ! Create window.
     display = x_open_display(c_null_char)
     screen  = x_default_screen(display)
+    root    = x_default_root_window(display)
 
-    root  = x_default_root_window(display)
     black = x_black_pixel(display, screen)
     white = x_white_pixel(display, screen)
 
     window = x_create_simple_window(display, root, 0, 0, 300, 200, 5, black, white)
 
-    gc = x_create_gc(display, window, 0, values)
+    ! Set window title.
+    call x_store_name(display, window, c_char_'Fortran' // c_null_char)
 
-    call x_select_input(display, window, ior(exposure_mask, key_press_mask));
+    ! Create graphics context.
+    gc = x_create_gc(display, window, 0, values)
 
     call x_set_background(display, gc, white)
     call x_set_foreground(display, gc, black)
 
+    ! Show window.
+    call x_select_input(display, window, ior(exposure_mask, ior(structure_notify_mask, key_press_mask)));
     call x_map_window(display, window)
-    call x_clear_window(display, window)
-    call x_sync(display, .false._c_bool)
 
+    wm_delete_window = x_intern_atom(display, 'WM_DELETE_WINDOW', .false._c_bool)
+    status           = x_set_wm_protocols(display, window, wm_delete_window, 1)
+
+    ! Event loop.
     do
-        write(*, *) 'waiting for event ...'
-        call x_next_event(display, c_loc(event))
-        write(*, '(a, i0)') 'event type: ', event%event_type
+        write(*, '(a)') 'waiting for event ...'
+        call x_next_event(display, event)
+
+        select case(event%type)
+            case(expose)
+                write(*, *) 'Expose'
+            case(configure_notify)
+                write(*, *) 'ConfigureNotify'
+                xce = event%xconfigure
+                write(*, *) xce%x
+                write(*, *) xce%y
+            case(client_message)
+                write(*, *) 'ClientMessage'
+            case(key_press)
+                write(*, *) 'KeyPress'
+        end select
     end do
 
+    ! Clean up and close window.
     call x_free_gc(display, gc)
     call x_destroy_window(display, window)
     call x_close_display(display)
--- examples/window.f90
+++ examples/window.f90
@@ -1,6 +1,6 @@
 ! window.f90
 !
-! Simple example application to create a window with xlib.
+! Simple example application to create a window with Xlib.
 !
 ! Author:  Philipp Engel
 ! Date:    2018-FEB-03
@@ -9,40 +9,59 @@
 ! Build with:
 ! $ gfortran8 -o window -Wl,-rpath=/usr/local/lib/gcc8/ -I/usr/local/include/ -L/usr/local/lib/ -lX11 window.f90 xlib.o
 program main
-    use iso_c_binding
+    use, intrinsic :: iso_c_binding
     use xlib
+    use xlib_consts
     use xlib_types
     implicit none
-    type(c_ptr)       :: display
-    type(c_ptr)       :: gc
-    type(x_gc_values) :: values
-    integer           :: screen
-    integer(kind=8)   :: root
-    integer(kind=8)   :: window
-    integer(kind=8)   :: black
-    integer(kind=8)   :: white
+    type(c_ptr)        :: display
+    type(c_ptr)        :: gc
+    type(x_gc_values)  :: values
+    type(x_size_hints) :: size_hints
+    integer            :: screen
+    integer            :: width  = 640
+    integer            :: height = 480
+    integer(kind=8)    :: root
+    integer(kind=8)    :: window
+    integer(kind=8)    :: black
+    integer(kind=8)    :: white
 
+    ! Create window.
     display = x_open_display(c_null_char)
     screen  = x_default_screen(display)
+    root    = x_default_root_window(display)
 
-    root  = x_default_root_window(display)
     black = x_black_pixel(display, screen)
     white = x_white_pixel(display, screen)
 
-    window = x_create_simple_window(display, root, 0, 0, 300, 200, 5, black, white)
+    window = x_create_simple_window(display, root, 0, 0, width, height, 5, black, white)
+
+    ! Prevent resizing.
+    size_hints%flags      = ior(p_min_size, p_max_size)
+    size_hints%min_width  = width
+    size_hints%min_height = height
+    size_hints%max_width  = width
+    size_hints%max_height = height
+
+    call x_set_wm_normal_hints(display, window, size_hints)
+
+    ! Set window title.
     call x_store_name(display, window, c_char_'Fortran' // c_null_char)
 
+    ! Create graphics context.
     gc = x_create_gc(display, window, 0, values)
 
     call x_set_background(display, gc, white)
     call x_set_foreground(display, gc, black)
 
+    ! Show window.
     call x_map_window(display, window)
     call x_clear_window(display, window)
     call x_sync(display, .false._c_bool)
 
-    call sleep(3)
+    call sleep(5)
 
+    ! Clean up and close window.
     call x_free_gc(display, gc)
     call x_destroy_window(display, window)
     call x_close_display(display)
